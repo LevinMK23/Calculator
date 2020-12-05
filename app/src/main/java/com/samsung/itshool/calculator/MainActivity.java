ac3c306dcf35408e4f45ac4c76b220e18c92d5ab
levinmk23 <levinmk23@gmail.com>
2020-12-05 18:28:14 +0300

    Урок 1

--- app/src/main/java/com/samsung/itshool/calculator/MainActivity.java
+++ app/src/main/java/com/samsung/itshool/calculator/MainActivity.java
@@ -2,40 +2,74 @@ package com.samsung.itshool.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText inputField;
-    private double left;
+    private EditText txt;
     private String operation;
+    private long left;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        inputField = findViewById(R.id.inputField);
-        Button plus = findViewById(R.id.plus);
-        plus.setOnClickListener(view -> {
-            left = Double.parseDouble(inputField.getText().toString());
-            operation = "+";
-            inputField.setText("");
+        txt = findViewById(R.id.txt);
+        Button one = findViewById(R.id.one);
+        one.setOnClickListener(view -> {
+
         });
-        Button equals = findViewById(R.id.eq);
-        equals.setOnClickListener(v -> {
-            double result = 0;
-            switch (operation) {
-                case "+":
-                    result = left + Double.parseDouble(inputField.getText().toString());
-            }
-            inputField.setText(String.valueOf(result));
-        });
+
+        Toast.makeText(getApplicationContext(), "TEXT", Toast.LENGTH_LONG).show();
+
     }
 
+    @SuppressLint("SetTextI18n")
     public void input(View view) {
-        inputField.append(((Button) view).getText());
+        Button btn = (Button) view;
+        String btnText = btn.getText().toString();
+        txt.setText(txt.getText() + btnText);
+    }
+
+    public void clear(View view) {
+        txt.setText("");
+    }
+
+    public void operate(View view) {
+        Button btn = (Button) view;
+        operation = btn.getText().toString();
+        left = getLong();
+        clear(view);
+    }
+
+    private long getLong() {
+        return Long.parseLong(txt.getText().toString());
+    }
+
+    public void calculate(View view) {
+        long result = 0;
+        switch (operation) {
+            case "+" -> {
+                result = left + getLong();
+                txt.setText(String.valueOf(result));
+            }
+            case "-" -> {
+                result = left - getLong();
+                txt.setText(String.valueOf(result));
+            }
+            case "*" -> {
+                result = left * getLong();
+                txt.setText(String.valueOf(result));
+            }
+            case "/" -> {
+                result = left / getLong();
+                txt.setText(String.valueOf(result));
+            }
+        }
     }
 }
